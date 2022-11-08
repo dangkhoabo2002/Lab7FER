26cee1f8cf48fe50836e160cd384fea4fc7da9c3
dangkhoabo2002 <dangkhoabo2002@gmail.com>
2022-11-09 00:24:24 +0700

    profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,82 @@
-import React from 'react'
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
 export default function Profile() {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
   return (
-    <div>Profile</div>
-  )
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        paddingTop: "10vh",
+      }}
+    >
+      <Card
+        sx={{
+          maxWidth: 345,
+        }}
+      >
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+              R
+            </Avatar>
+          }
+          title="Shrimp and Chorizo Paella"
+          subheader="September 14, 2016"
+        />
+        <CardMedia
+          component="img"
+          height="194"
+          image="/static/images/cards/paella.jpg"
+          alt="Paella dish"
+        />
+        <CardContent sx={{ textAlign: "left" }}>
+          <Typography variant="body2" color="text.secondary">
+            Name :
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Gmail :
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Date of Birth :
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Phone :
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
